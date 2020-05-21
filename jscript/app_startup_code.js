b6aa0def988c8a54915066b624e98dfff875a3ed
Abhinav Srivastava <abhinav.022000@gmail.com>
2020-05-21 23:40:02 +0530

    links update

--- jscript/app_startup_code.js
+++ jscript/app_startup_code.js
@@ -33,8 +33,8 @@ function predictOnLoad() {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('http://skin.test.woza.work/final_model_kaggle_version1/model.json');
-	$("#selected-image").attr("src", "http://skin.test.woza.work/assets/samplepic.jpg");
+	model = await tf.loadModel('https://stupefied-spence-7a6c28.netlify.app/final_model_kaggle_version1/model.json');
+	$("#selected-image").attr("src", "https://stupefied-spence-7a6c28.netlify.app/assets/samplepic.jpg");
 	
 	// Hide the model loading spinner
 	// This line of html gets hidden:
