586f6e45559930adc29e1a1752103fff34d08358
Abhinav Srivastava <abhinav.022000@gmail.com>
2020-05-25 11:40:50 +0530

    link update

--- jscript/app_startup_code.js
+++ jscript/app_startup_code.js
@@ -33,8 +33,8 @@ function predictOnLoad() {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('https://stupefied-spence-7a6c28.netlify.app/final_model_kaggle_version1/model.json');
-	$("#selected-image").attr("src", "https://stupefied-spence-7a6c28.netlify.app/assets/samplepic.jpg");
+	model = await tf.loadModel('https://abhinavsri360.github.io/Skin-Lesion-Analyzer/final_model_kaggle_version1/model.json');
+	$("#selected-image").attr("src", "https://abhinavsri360.github.io/Skin-Lesion-Analyzer/assets/samplepic.jpg");
 	
 	// Hide the model loading spinner
 	// This line of html gets hidden:
